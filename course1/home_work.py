8880769bf4dd8ee50787da161f1ca12fce7d43eb
dimak1986 <dimak@tabtale.com>
2018-09-06 21:29:20 +0300

    home_work1

---
+++ course1/home_work.py
@@ -0,0 +1,27 @@
+
+q = input('what is the lang of Isael: ')
+if q != 'Hebrew':
+    print('Not at all!')
+    q = input('what is the lang of Isael: ')
+if q == 'Hebrew':
+    print("you're right")
+
+print('you need to give paraments to matter a square')
+
+y = int(input('give a number for y: '))
+
+x = int(input('give a number for x: '))
+
+answer = int(x * y)
+
+print(answer)
+
+
+while True:
+    q2 = input('How much will be 5 + 5:')
+    if float(q2) == 10:
+        print('good = %s' % q2)
+        break
+    else:
+        print('%s you are wrong' % q2)
+        continue
