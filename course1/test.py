7e308bd5888bbb88940c9ee2d3fb3a776bcfe82b
dimak1986 <dimak@tabtale.com>
2018-09-09 21:36:02 +0300

    course2

---
+++ course1/test.py
@@ -0,0 +1,23 @@
+d = 'dima'
+l = 'lena'
+
+while True:
+    name = input('what is your name: ')
+    if l == name:
+        print('good ')
+        break
+
+    else:
+        print('no ')
+        continue
+
+
+while True:
+    name2 = input('what the name of your love: ')
+    if d == name2:
+        print('you are right')
+        break
+
+    else:
+        print('you wrong')
+        continue
