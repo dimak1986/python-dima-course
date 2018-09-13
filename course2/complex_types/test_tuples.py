d8560c6c073ee4376e2520e12f474db22fc64693
dimak1986 <dimak@tabtale.com>
2018-09-13 17:09:15 +0300

    13.09 changes to test

--- course2/complex_types/test_tuples.py
+++ course2/complex_types/test_tuples.py
@@ -13,9 +13,9 @@ import copy
 
 var = {'list': [[1, 2], ['a', 'b']], 'tuple': ('foo', 'bar', )}
 new_var = copy.deepcopy(var)
-var.update({'str': 'string'})
-var['list'].append([True, False])
-var['tuple'] += ('baz', )
+var.update({'str': 'string'}) #NEw key and value added
+var['list'].append([True, False]) #in dict: 'list' (key) added another list
+var['tuple'] += ('baz', ) # New string added to existing 'tuple'
 
 print(var)
 print(new_var)
